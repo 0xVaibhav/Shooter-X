a57694651b7caaa8b20257ec179ac664c659da52
vaibhav <vaibhav0240gupta@gmail.com>
2025-02-13 16:19:14 +0530

    EnemyHealthPoints_set

---
+++ Assets/Script/EnemyHealth.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    int currentHealth;
+    [SerializeField] int startHealth = 3;
+
+    void Awake()
+    {
+        currentHealth = startHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
--- Assets/Script/Weapon.cs
+++ Assets/Script/Weapon.cs
@@ -1,24 +1,26 @@
-using System;
+
 using StarterAssets;
 using UnityEngine;
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] int damageAmount = 1;
     StarterAssetsInputs starterAssetsInputs;
-
-    private void Awake()
+    void Awake()
     {
         starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
     }
     void Update()
     {
-        if (starterAssetsInputs.shoot)
+        HandleShoot();
+    }
+    void HandleShoot()
+    {
+        if (!starterAssetsInputs.shoot) return;
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
-            RaycastHit hit;
-
-            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
-            {
-                Debug.Log(hit.collider.name);
-            };
+            EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
+            enemyHealth?.TakeDamage(damageAmount);
             starterAssetsInputs.ShootInput(false);
         }
     }
