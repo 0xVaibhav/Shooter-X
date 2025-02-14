769a5ec6eba453e92e7fa4edb8d362168ad1a80d
vaibhav <vaibhav0240gupta@gmail.com>
2025-02-14 11:59:50 +0530

    muzzle_flash

--- Assets/Script/Weapon.cs
+++ Assets/Script/Weapon.cs
@@ -3,6 +3,7 @@ using StarterAssets;
 using UnityEngine;
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] ParticleSystem muzzleFlash;
     [SerializeField] int damageAmount = 1;
     StarterAssetsInputs starterAssetsInputs;
     void Awake()
@@ -16,6 +17,7 @@ public class Weapon : MonoBehaviour
     void HandleShoot()
     {
         if (!starterAssetsInputs.shoot) return;
+        muzzleFlash.Play();
         RaycastHit hit;
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
