738265290af942660acbec86aed212a73824d18e
vaibhav <vaibhav0240gupta@gmail.com>
2025-02-14 13:20:09 +0530

    Weapon_recoil_animation

--- Assets/Script/Weapon.cs
+++ Assets/Script/Weapon.cs
@@ -3,9 +3,11 @@ using StarterAssets;
 using UnityEngine;
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] Animator animator;
     [SerializeField] ParticleSystem muzzleFlash;
     [SerializeField] int damageAmount = 1;
     StarterAssetsInputs starterAssetsInputs;
+    const string SHOOT_STRING = "Shoot";
     void Awake()
     {
         starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
@@ -18,12 +20,13 @@ public class Weapon : MonoBehaviour
     {
         if (!starterAssetsInputs.shoot) return;
         muzzleFlash.Play();
+        animator.Play(SHOOT_STRING, 0, 0f);
+        starterAssetsInputs.ShootInput(false);
         RaycastHit hit;
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
             enemyHealth?.TakeDamage(damageAmount);
-            starterAssetsInputs.ShootInput(false);
         }
     }
 }
